7cd91edd4b1359f850838954a79934e01a805994
dkirshenbaum <davidk@iastate.edu>
2018-03-24 19:06:12 -0500

    Continuing with being a matchmaker

--- src/SwapListener.js
+++ src/SwapListener.js
@@ -19,11 +19,39 @@ server.deploy(config);
 async function handleSwaps(request,response,serve) {
   console.log('hit');
   let swapQuery = request.body ? JSON.parse(request.body) : undefined;
-  let productId = swapQuery && swapQuery.productId ? swapQuery.productId : undefined;
-  
+  let productId = swapQuery && swapQuery.productID ? swapQuery.productID : undefined;
+ // console.log(swapQuery);
+  //console.log(productId);
   if(swapQuery && productId) {
-    let swaps = await firebase.list('/products/' + id + '/swaps/');
-    console.log(swaps);
+    let swaps = await firebase.database().ref('/products/' + productId+ '/swaps/').once("value"); // .orderByChild('in');
+    console.log(typeof swaps);
+    let swapReturn = swaps.exportVal();
+    let foundSwap = false;
+    Object.keys(swapReturn).forEach(async(key) =>{
+      let current = swapReturn[key];
+      if(current.in === swapQuery.out && current.out === swapQuery.in){
+        foundSwap = true;
+        let requesterSize = current.in;
+        let buyerSize = swapQuery.in;
+        let requesterId = current.user;
+        let buyerId = swapQuery.user;
+        let match = {
+          requesterSize,
+          buyerSize,
+          requesterId,
+          buyerId
+        }
+        let result = await firebase.database().ref('/pendingSwaps').push(match);
+      }
+    });
+    if(!foundSwap){
+      //Add new swap request to DB
+    }
+    
+    // console.log(swaps.getValue());
+    
+    
+
   }
   return JSON.stringify({
     statusCode: 200
